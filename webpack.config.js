67fce2b2f8a4bdcb2244aa1a2de3d840f9858a00
GaliullinAR <galiullin.ar651@gmail.com>
2022-09-07 00:22:03 +0300

    change library name in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    library: 'platform-at-ajs',
+    library: 'ajs',
     libraryTarget: 'umd',
     libraryExport: 'default',
     globalObject: 'this'
